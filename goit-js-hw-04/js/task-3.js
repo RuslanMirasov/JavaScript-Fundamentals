f73e582b2c7fe7d359c851b4f3b4f06f1ea175f0
Ruslan <info@mirasov.dev>
2026-07-27 02:45:33 +0200

    add hw4 structure

---
+++ goit-js-hw-04/js/task-3.js
@@ -0,0 +1,30 @@
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
+
+  getInfo() {
+    // TODO
+
+    return `TODO`;
+  },
+
+  changeUsername(newName) {
+    // TODO
+
+    return `TODO`;
+  },
+
+  updatePlayTime(hours) {
+    // TODO
+
+    return `TODO`;
+  },
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
